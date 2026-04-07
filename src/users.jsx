e06c243c96a2fe742ece885871cb43a0f37eac84
pragna21-dev <pragna21.patel@gmail.com>
2026-04-07 10:45:48 +0530

    Usestate array Obj list

--- src/users.jsx
+++ src/users.jsx
@@ -3,36 +3,64 @@ import { useState } from "react";
 function Users() {
   const [users, setUsers] = useState([]);
   const [userName, setName] = useState("");
-  const addUser = ()=>{
-    setUsers([...users,userName]);
+  const [userObj, setUserObj] = useState({ name: "", age: "" });
+  const [userObjArray, setUserObjarray] = useState([]);
+
+  const addUser = () => {
+    setUsers([...users, userName]);
     setName("");
-  }
-  const deleteUser = (index)=>{
+  };
+  const addUserArray = () => {
+    setUserObjarray([...userObjArray, userObj]);
+    setUserObj({ name: "", age: "" });
+  };
+
+  const deleteUser = (index) => {
     //users.splice(index,1);
-    const newUsers=users.filter((val,i)=>i!=index)
+    const newUsers = users.filter((val, i) => i != index);
     console.log(newUsers);
-     setUsers(newUsers);
-    
-  }
+    setUsers(newUsers);
+  };
+
   return (
     <>
       <h1>Users</h1>
-      <div style={{display:"flex", gap:"10px"}}>
-      <input
-        type="text"
-        value={userName}
-        onChange={(e) => setName(e.target.value)}
-      ></input>
-      <button onClick={addUser}>Add Users</button>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <input
+          type="text"
+          value={userName}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <button onClick={addUser}>Add Users</button>
       </div>
       Hello, {userName},
-      {users.map((item,index)=>(
-        <li key={index}>{item}
-        <button onClick={()=>deleteUser(index)}>Delet</button>
+      {users.map((item, index) => (
+        <li key={index}>
+          {item}
+          <button onClick={() => deleteUser(index)}>Delet</button>
         </li>
-      )
-
-      )}
+      ))}
+      <p>User Object</p>
+      <input
+        type="text"
+        value={userObj.name}
+        onChange={(e) => setUserObj({ ...userObj, name: e.target.value })}
+      ></input>
+     
+      <input
+        type="text"
+        value={userObj.age}
+        onChange={(e) => setUserObj({ ...userObj, age: e.target.value })}
+      ></input>
+      <button onClick={addUserArray}>Add User</button>
+       <p>User obj array</p>
+      <ul>
+        {userObjArray.map((obj, index) => (
+          <li key={index}>
+            {obj.name} - {obj.age}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
